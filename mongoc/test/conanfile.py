d0b590178a1f61afa5bcf354a9432503f55d4537
Pooya Daravi <daravi@alumni.ubc.ca>
2019-06-10 01:42:41 -0700

    remove header_path.patch

--- mongoc/test/conanfile.py
+++ mongoc/test/conanfile.py
@@ -9,15 +9,15 @@ class MongoCDriverConan(ConanFile):
 	version = "1.14.0"
 	description = "A high-performance MongoDB driver for C"
 	topics = ("libmongoc", "mongodb", "db")
-	license = "Apache-2.0"
 	homepage = "https://github.com/mongodb/{0}".format(name)
+	license = "Apache-2.0"
 	url = "https://github.com/mongodb/{0}/releases/download/{1}/{0}-{1}.tar.gz".format(name, version)
-	
-	settings = "os", "compiler", "arch", "build_type"
-	generators = "cmake"
 	exports_sources = ["Find*.cmake", "CMakeLists.txt"]
-	options = {"shared": [True, False], "fPIC": [True, False]}
-	default_options = {"shared": False, "fPIC": True}
+	generators = "cmake"
+
+	settings = "os", "compiler", "arch", "build_type"
+	options = {"enable_static": [True, False], "fPIC": [True, False]}
+	default_options = {"enable_static": True, "fPIC": True}
 
 	_source_subfolder = "source_subfolder"
 	_build_subfolder = "build_subfolder"
@@ -25,9 +25,8 @@ class MongoCDriverConan(ConanFile):
 	requires = "zlib/1.2.11@conan/stable"
 
 	def configure(self):
-		# Because this is pure C and does not depend on any C++ standard library
+		# Because this is pure C
 		del self.settings.compiler.libcxx
-		del self.settings.compiler.cppstd
 
 	def config_options(self):
 		if self.settings.os == "Windows":
@@ -41,6 +40,7 @@ class MongoCDriverConan(ConanFile):
 		tools.get(self.url)
 		extracted_dir = self.name + "-" + self.version
 		os.rename(extracted_dir, self._source_subfolder)
+		# tools.patch(base_path=self._source_subfolder, patch_file="header_path.patch")
 
 	def _configure_cmake(self):
 		cmake = CMake(self)
@@ -49,7 +49,7 @@ class MongoCDriverConan(ConanFile):
 		cmake.definitions["ENABLE_AUTOMATIC_INIT_AND_CLEANUP"] = False
 		cmake.definitions["ENABLE_BSON"] = "ON"
 		cmake.definitions["ENABLE_SASL"] = "OFF"
-		cmake.definitions["ENABLE_STATIC"] = "OFF" if self.options.shared else "ON"
+		cmake.definitions["ENABLE_STATIC"] = "ON" if self.options.enable_static else "OFF"
 
 		cmake.configure(build_folder=self._build_subfolder)
 
@@ -57,10 +57,9 @@ class MongoCDriverConan(ConanFile):
 
 	def build(self):
 		cmake = self._configure_cmake()
-		if self.options.shared:
-			cmake.build(target="bson_shared")
-			cmake.build(target="mongoc_shared")
-		else:
+		cmake.build(target="bson_shared")
+		cmake.build(target="mongoc_shared")
+		if self.options.enable_static:
 			cmake.build(target="bson_static")
 			cmake.build(target="mongoc_static")
 		# cmake.build()
@@ -84,7 +83,7 @@ class MongoCDriverConan(ConanFile):
 		if tools.os_info.is_linux:
 			self.cpp_info.libs.extend(["rt", "pthread"])
 
-		if not self.options.shared:
+		if self.options.enable_static:
 			self.cpp_info.defines.extend(['BSON_STATIC=1', 'MONGOC_STATIC=1'])
 
 			if tools.os_info.is_linux or tools.os_info.is_macos:
--- mongocxx/conanfile.py
+++ mongocxx/conanfile.py
@@ -11,88 +11,55 @@ class MongoCxxConan(ConanFile):
     version = "3.4.0"
     description = "C++ Driver for MongoDB"
     topics = ("libmongocxx", "mongodb", "db")
-    homepage = "https://github.com/mongodb/{0}".format(name)
     license = "Apache-2.0"
+    homepage = "https://github.com/mongodb/{0}".format(name)
     url = "https://github.com/mongodb/{0}/archive/r{1}.tar.gz".format(name, version)
-    generators = "cmake"
 
     settings =  "os", "compiler", "arch", "build_type"
+    generators = "cmake"
+    exports_sources = ["CMakeLists.txt"]
     options = {"shared": [True, False]}
-    default_options = "shared=False"
+    default_options = {"shared": False}
 
-    requires = 'mongo-c-driver/1.14.0@3rdparty/stable'
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
+
+    requires = 'mongo-c-driver/1.14.0@test/test'
 
     def source(self):
+        # tools.get(self.url)
+        # extracted_dir = "mongo-cxx-driver-r{0}".format(self.version)
+        # os.rename(extracted_dir, "sources")
         tools.get(self.url)
-        extracted_dir = "mongo-cxx-driver-r{0}".format(self.version)
-        os.rename(extracted_dir, "sources")
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
 
     def build(self):
-        conan_magic_lines='''project(MONGO_CXX_DRIVER LANGUAGES CXX)
-        include(../conanbuildinfo.cmake)
-        conan_basic_setup()
-        # include(CheckCXXCompilerFlag)
-        # check_cxx_compiler_flag(-std=c++17 HAVE_FLAG_STD_CXX17)
-        # if(HAVE_FLAG_STD_CXX17)
-        #     message(STATUS USING C++17!)
-        #     set(CMAKE_CXX_STANDARD 17)
-        #     set(BSONCXX_POLY_USE_MNMLSTC 0)
-        #     set(BSONCXX_POLY_USE_STD_EXPERIMENTAL 0)
-        #     set(BSONCXX_POLY_USE_BOOST 0)
-        #     set(BSONCXX_POLY_USE_STD 1)
-        # else()
-        #     check_cxx_compiler_flag(-std=c++14 HAVE_FLAG_STD_CXX14)
-        #     if(HAVE_FLAG_STD_CXX14)
-        #         message(STATUS USING C++14!)
-        #         set(CMAKE_CXX_STANDARD 14)
-        #         set(BSONCXX_POLY_USE_MNMLSTC 0)
-        #         set(BSONCXX_POLY_USE_STD_EXPERIMENTAL 1)
-        #         set(BSONCXX_POLY_USE_BOOST 0)
-        #         set(BSONCXX_POLY_USE_STD 0)
-        #     else()
-        #         message(FATAL_ERROR "MongoCXX requires at least C++14")
-        #     endif()
-        # endif()
-        # set(CMAKE_CXX_STANDARD_REQUIRED ON)
-        # set(CMAKE_CXX_EXTENSIONS OFF)
-        set(BSONCXX_POLY_USE_MNMLSTC 1)
-        set(BSONCXX_POLY_USE_STD_EXPERIMENTAL 0)
-        set(BSONCXX_POLY_USE_BOOST 0)
-        set(BSONCXX_POLY_USE_STD 0)
-
-        set(CMAKE_CXX_STANDARD 17)
-        set(CMAKE_CXX_STANDARD_REQUIRED ON)
-        '''
-        
-        cmake_file = "sources/CMakeLists.txt"
-        tools.replace_in_file(cmake_file, "project(MONGO_CXX_DRIVER LANGUAGES CXX)", conan_magic_lines)
-        tools.replace_in_file(cmake_file, "enable_testing()", "")
-
+        cmake_file = self._source_subfolder + "/CMakeLists.txt"
         cmake = CMake(self)
+        # If C++17 not available:
         # if self.settings.compiler == 'Visual Studio':
         # 	cmake.definitions["BSONCXX_POLY_USE_BOOST"] = 1 # required for Windows.
-        cmake.definitions["BUILD_TESTING"] = 0
-        cmake.definitions["CMAKE_CXX_STANDARD"] = 17
+        # else:
+        #     cmake.definitions["BSONCXX_POLY_USE_MNMLSTC"] = 1
         cmake.definitions["CMAKE_CXX_STANDARD_REQUIRED"] = 1
-        cmake.definitions["BSONCXX_POLY_USE_MNMLSTC"] = 1
-        cmake.definitions["BSONCXX_POLY_USE_STD_EXPERIMENTAL"] = 0
-        cmake.definitions["BSONCXX_POLY_USE_BOOST"] = 0
-        cmake.definitions["BSONCXX_POLY_USE_STD"] = 0
-        cmake.configure(source_dir="sources")
-
+        cmake.definitions["CMAKE_CXX_STANDARD"] = 17
+        cmake.definitions["BSONCXX_POLY_USE_STD"] = 1
+        cmake.definitions["BUILD_TESTING"] = 0
+        cmake.configure(source_dir=self._source_subfolder)
         cmake.build(target="bsoncxx")
         cmake.build(target="mongocxx")
         cmake.build(target="mongocxx_mocked")
 
-    def purge(self, dir, pattern):
-        for f in os.listdir(dir):
-            if re.search(pattern, f):
-                os.remove(os.path.join(dir, f))
+    # def purge(self, dir, pattern):
+    #     for f in os.listdir(dir):
+    #         if re.search(pattern, f):
+    #             os.remove(os.path.join(dir, f))
 
     def package(self):
-        self.copy(pattern="LICENSE*", dst="licenses", src="sources")
-        self.copy(pattern="*.hpp", dst="include/bsoncxx", src="sources/src/bsoncxx", keep_path=True)
-        self.copy(pattern="*.hpp", dst="include/mongocxx", src="sources/src/mongocxx", keep_path=True)
+        self.copy(pattern="LICENSE*", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="*.hpp", dst="include/bsoncxx", src=self._source_subfolder + "/src/bsoncxx", keep_path=True)
+        self.copy(pattern="*.hpp", dst="include/mongocxx", src=self._source_subfolder + "/src/mongocxx", keep_path=True)
         self.copy(pattern="*.hpp", dst="include/bsoncxx", src="src/bsoncxx", keep_path=True)
         self.copy(pattern="*.hpp", dst="include/mongocxx", src="src/mongocxx", keep_path=True)
         self.copy(pattern="*.hpp", dst="include/bsoncxx/third_party/mnmlstc/core", src="src/bsoncxx/third_party/EP_mnmlstc_core-prefix/src/EP_mnmlstc_core/include/core", keep_path=False)
